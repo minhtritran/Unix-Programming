1fdc3aa5df2647db4f688585bf46cfbdcf69b27b
Minhtri Tran <mtran7305@gmail.com>
2015-11-23 19:04:27 -0500

    Finished HW7

--- HW7/hw7-client.c
+++ HW7/hw7-client.c
@@ -1,7 +1,7 @@
 //Minhtri Tran
 //Unix Programming
 //Assignment 7 - Networks and I/O Multiplexing
-//Client
+//1-to-1 Chat Client
 
 #include <stdio.h>
 #include <string.h>
@@ -10,56 +10,29 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <signal.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <sys/select.h>
+#include <stdbool.h>
 
-#define SERVER_IP "127.0.0.1"
 #define BUFFER_SIZE 1024
 #define TIMEOUT_SECS 180
 
+void handleInput(int argc, char* argv[], char** client_name, char* server_ip, int* server_port);
+int connectToServer(char* server_ip, int server_port);
+bool handleOutgoingMsg(int sock_fd, char* msg_buf, char* client_name);
+bool handleIncomingMsg(int sock_fd, char* msg_buf);
+
 int main(int argc, char *argv[]) {
-	//handle command arguments
-	char* client_name = malloc(sizeof(char) * BUFFER_SIZE);
-	if (client_name == NULL) {
-		perror("client_name malloc failed");
-		exit(1);
-	}
-	int server_port = 13000;
-	if (argc <= 1) {
-		printf("Usage: client name [port]\n");
-		exit(1);
-	}
-	if (argc >= 2) {
-		client_name = argv[1];
-	}
+	char* client_name;
+	char server_ip[BUFFER_SIZE];
+	int server_port;
+	handleInput(argc, argv, &client_name, server_ip, &server_port);
 
-	//create socket
-	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock_fd == -1) {
-		perror("socket failed");
-		exit(1);
-	}
-	
-	//set up server info
-	struct sockaddr_in servaddr;
-	memset(&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(server_port);
-	if (inet_pton(AF_INET, SERVER_IP, &servaddr.sin_addr) <= 0) {
-		perror("inet_pton failed");
-		exit(1);
-	}
-
-	//connect to server
-	if (connect(sock_fd, (struct sockaddr *) &servaddr, sizeof(servaddr)) == -1) {
-		perror("connect failed");
-		exit(1);
-	}
-
+	int sock_fd = connectToServer(server_ip, server_port);
+	printf("Connection established. Start chatting!\n");
 
 	while (1) {
 		//select
@@ -78,32 +51,131 @@ int main(int argc, char *argv[]) {
 		}
 
 		//if fd == 0 means timeout
-		char child_buf[BUFFER_SIZE];
-		int n = 0;
+		char msg_buf[BUFFER_SIZE];
+		memset(&msg_buf, 0, sizeof(msg_buf));
 		if (FD_ISSET(STDIN_FILENO, &set)) {
-			n = read(STDIN_FILENO, child_buf, BUFFER_SIZE);
-
-			char client_name_buf[BUFFER_SIZE];
-			int client_name_size = snprintf(client_name_buf, BUFFER_SIZE, "%s: ", client_name);
-			if (write(sock_fd, client_name_buf, client_name_size) == -1) {
-				perror("write failed");
-				exit(1);
-			}
-			if (write(sock_fd, child_buf, n) == -1) {
-				perror("write failed");
-				exit(1);
+			if (!handleOutgoingMsg(sock_fd, msg_buf, client_name)) {
+				printf("Quitting..\n");
+				break;
 			}
 		}
 		if (FD_ISSET(sock_fd, &set)) {
-			n = read(sock_fd, child_buf, BUFFER_SIZE);
-			if (write(STDOUT_FILENO, child_buf, n) == -1) {
-				perror("write failed");
-				exit(1);
+			if (!handleIncomingMsg(sock_fd, msg_buf)) {
+				printf("Server closed.\n");
+				break;
 			}
 		}
 	}
 
+	free(client_name);
 	close(sock_fd);
 
 	return 0;
+}
+
+//Handle command arguments
+void handleInput(int argc, char* argv[], char** client_name, char* server_ip, int* server_port) {
+	*client_name = malloc(sizeof(char) * BUFFER_SIZE);
+	if (*client_name == NULL) {
+		perror("client_name malloc failed");
+		exit(1);
+	}
+	if (argc <= 3) {
+		printf("Usage: client name ip port\n");
+		exit(1);
+	}
+	if (argc >= 2) {
+		strcpy(*client_name, argv[1]);
+	}
+	if (argc >= 3) {
+		strcpy(server_ip, argv[2]);
+	}
+	if (argc >= 4) {
+		char* endptr;
+		*server_port = strtol(argv[3], &endptr, 10);
+		if (*endptr != '\0') {
+			fprintf(stderr, "%s is not a valid port.\n", argv[3]);
+			exit(1);
+		}
+	}
+}
+
+//Connect to the given server ip and port
+//Returns a socket file descriptor
+int connectToServer(char* server_ip, int server_port) {
+	//create socket
+	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock_fd == -1) {
+		perror("socket failed");
+		exit(1);
+	}
+	
+	//set up server info
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(server_port);
+	int pton_return_val = inet_pton(AF_INET, server_ip, &servaddr.sin_addr);
+	if (pton_return_val == 0) {
+		fprintf(stderr, "%s is an invalid ip address.\n", server_ip);
+		exit(1);
+	}
+	else if (pton_return_val == -1) {
+		perror("inet_pton failed");
+		exit(1);
+	}
+
+	//connect to server
+	if (connect(sock_fd, (struct sockaddr *) &servaddr, sizeof(servaddr)) == -1) {
+		perror("connect failed");
+		exit(1);
+	}
+
+	return sock_fd;
+}
+
+//Handle outgoing messages
+//Returns false if client (you) sends a 'quit' message, true otherwise
+bool handleOutgoingMsg(int sock_fd, char* msg_buf, char* client_name) {
+	//read from socket
+	int n = read(STDIN_FILENO, msg_buf, BUFFER_SIZE);
+
+	//handle client quitting
+	if (strcmp(msg_buf, "quit\n") == 0) {
+		if (write(sock_fd, msg_buf, n) == -1) {
+			perror("write failed");
+			exit(1);
+		}
+		close(sock_fd);
+		return false;
+	}
+
+	//write message to socket
+	char client_message[BUFFER_SIZE];
+	int client_message_n = snprintf(client_message, BUFFER_SIZE, "%s: %s", client_name, msg_buf);
+	if (write(sock_fd, client_message, client_message_n) == -1) {
+		perror("write failed");
+		exit(1);
+	}
+
+	return true;
+}
+
+//Handle incoming messages
+//Returns false if server sends a 'quit' message, true otherwise
+bool handleIncomingMsg(int sock_fd, char* msg_buf) {
+	int n = read(sock_fd, msg_buf, BUFFER_SIZE);
+
+	//handle server quitting
+	if (strcmp(msg_buf, "quit\n") == 0) {
+		close(sock_fd);
+		return false;
+	}
+
+	if (write(STDOUT_FILENO, msg_buf, n) == -1) {
+		perror("write failed");
+		exit(1);
+	}
+
+	return true;
 }
--- HW7/hw7-server.c
+++ HW7/hw7-server.c
@@ -1,7 +1,7 @@
 //Minhtri Tran
 //Unix Programming
 //Assignment 7 - Networks and I/O Multiplexing
-//Server
+//1-to-1 Chat Server
 
 #include <stdio.h>
 #include <string.h>
@@ -13,28 +13,94 @@
 #include <signal.h>
 #include <netdb.h>
 #include <sys/select.h>
+#include <stdbool.h>
 
-#define	LISTENQ  1024
+#define	LISTENQ 1024
 #define BUFFER_SIZE 1024
 #define TIMEOUT_SECS 180
 #define DEFAULT_PORT 13000
 
+void handleInput(int argc, char* argv[], char** server_name, int* server_port);
+int setUpServer(int server_port);
+bool handleOutgoingMsg(int sock_fd, char* msg_buf, char* server_name);
+bool handleIncomingMsg(int sock_fd, char* msg_buf);
+
 int main(int argc, char *argv[]) {
-	//handle command arguments
-	char* server_name = malloc(sizeof(char) * BUFFER_SIZE);
-	if (server_name == NULL) {
+	char* server_name;
+	int server_port;
+	handleInput(argc, argv, &server_name, &server_port);
+
+	int server_fd = setUpServer(server_port);
+
+	while (1) {
+		//accept
+		printf("Waiting for client connection..\n");
+		int sock_fd = accept(server_fd, (struct sockaddr *) NULL, NULL);
+		printf("Connection established. Start chatting!\n");
+		while (1) {
+			//select
+			fd_set set;
+			FD_ZERO(&set);
+			int max_fd = sock_fd + 1;
+			struct timeval timeout;
+			timeout.tv_sec = TIMEOUT_SECS;
+			timeout.tv_usec = 0;
+
+			FD_SET(STDIN_FILENO, &set);
+			FD_SET(sock_fd, &set);
+			if (select(max_fd, &set, NULL, NULL, &timeout) == -1) {
+				perror("select failed");
+				exit(1);
+			}
+
+			char msg_buf[BUFFER_SIZE];
+			memset(&msg_buf, 0, sizeof(msg_buf));
+			if (FD_ISSET(STDIN_FILENO, &set)) {
+				if (!handleOutgoingMsg(sock_fd, msg_buf, server_name)) {
+					printf("Closing server..\n");
+					free(server_name);
+					exit(0);
+				}
+			}
+			if (FD_ISSET(sock_fd, &set)) {
+				if (!handleIncomingMsg(sock_fd, msg_buf)) {
+					printf("Connection ended.\n");
+					break;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+//Handle command arguments
+void handleInput(int argc, char* argv[], char** server_name, int* server_port) {
+	*server_name = malloc(sizeof(char) * BUFFER_SIZE);
+	if (*server_name == NULL) {
 		perror("server_name malloc failed");
 		exit(1);
 	}
-	int server_port = DEFAULT_PORT;
+	*server_port = DEFAULT_PORT;
 	if (argc <= 1) {
 		printf("Usage: server name [port]\n");
 		exit(1);
 	}
 	if (argc >= 2) {
-		server_name = argv[1];
+		strcpy(*server_name, argv[1]);
 	}
+	if (argc >= 3) {
+		char* endptr;
+		*server_port = strtol(argv[2], &endptr, 10);
+		if (*endptr != '\0') {
+			fprintf(stderr, "%s is not a valid port.  Defaulting to port %d.\n", argv[2], DEFAULT_PORT);
+			*server_port = DEFAULT_PORT;
+		}
+	}
+}
 
+//Set up server using the given port
+//Returns the server file descriptor
+int setUpServer(int server_port) {
 	//create socket
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
@@ -61,51 +127,49 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
-	//accept
-	int sock_fd = accept(server_fd, (struct sockaddr *) NULL, NULL);
-	while (1) {
-		//select
-		fd_set set;
-		FD_ZERO(&set);
-		int max_fd = sock_fd + 1;
-		struct timeval timeout;
-		timeout.tv_sec = TIMEOUT_SECS;
-		timeout.tv_usec = 0;
+	return server_fd;
+}
 
-		FD_SET(STDIN_FILENO, &set);
-		FD_SET(sock_fd, &set);
-		if (select(max_fd, &set, NULL, NULL, &timeout) == -1) {
-			perror("select failed");
+//Handle outgoing messages
+//Returns false if server (you) sends a 'quit' message, true otherwise
+bool handleOutgoingMsg(int sock_fd, char* msg_buf, char* server_name) {
+	int n = read(STDIN_FILENO, msg_buf, BUFFER_SIZE);
+
+	//handle server quitting
+	if (strcmp(msg_buf, "quit\n") == 0) {
+		if (write(sock_fd, msg_buf, n) == -1) {
+			perror("write failed");
 			exit(1);
 		}
-
-		char child_buf[BUFFER_SIZE];
-		int n = 0;
-		if (FD_ISSET(STDIN_FILENO, &set)) {
-			n = read(STDIN_FILENO, child_buf, BUFFER_SIZE);
-
-			char server_name_buf[BUFFER_SIZE];
-			int server_name_size = snprintf(server_name_buf, BUFFER_SIZE, "%s: ", server_name);
-			if (write(sock_fd, server_name_buf, server_name_size) == -1) {
-				perror("write failed");
-				exit(1);
-			}
-			if (write(sock_fd, child_buf, n) == -1) {
-				perror("write failed");
-				exit(1);
-			}
-		}
-		if (FD_ISSET(sock_fd, &set)) {
-			n = read(sock_fd, child_buf, BUFFER_SIZE);
-			if (write(STDOUT_FILENO, child_buf, n) == -1) {
-				perror("write failed");
-				exit(1);
-			}
-		}
-
+		close(sock_fd);
+		return false;
 	}
-	close(sock_fd);
 
+	char server_message[BUFFER_SIZE];
+	int server_message_n = snprintf(server_message, BUFFER_SIZE, "%s: %s", server_name, msg_buf);
+	if (write(sock_fd, server_message, server_message_n) == -1) {
+		perror("write failed");
+		exit(1);
+	}
 
-	return 0;
+	return true;
+}
+
+//Handle incoming messages
+//Returns false if client sends a 'quit' message, true otherwise
+bool handleIncomingMsg(int sock_fd, char* msg_buf) {
+	int n = read(sock_fd, msg_buf, BUFFER_SIZE);
+
+	//handle client quitting
+	if (strcmp(msg_buf, "quit\n") == 0) {
+		close(sock_fd);
+		return false;
+	}
+
+	if (write(STDOUT_FILENO, msg_buf, n) == -1) {
+		perror("write failed");
+		exit(1);
+	}
+
+	return true;
 }
